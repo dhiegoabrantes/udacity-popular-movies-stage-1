ccb276ecb3282d4db35b3abd5f967f29ad4bcdd0
Dhiego Abrantes <dhiegoabrantes@gmail.com>
2016-06-19 00:47:43 -0300

    Commit inicial.

---
+++ app/src/main/java/education/udacity/com/popularmovies_stage1/MainActivity.java
@@ -0,0 +1,13 @@
+package education.udacity.com.popularmovies_stage1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
